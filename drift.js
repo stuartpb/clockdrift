cc65b5f6b2cb045fa5406580bb401acae4e6cf7c
Stuart P. Bentley <stuart@testtrack4.com>
2016-03-14 04:16:55 -0700

    Update drift.js

--- drift.js
+++ drift.js
@@ -3,3 +3,5 @@ var microwaveRatio = 1 + (224000 / // 4 minutes slow, less 17 seconds
   +(new Date('02:47 2016-03-14 UTC'))
   // date set
   - +(new Date('17:36 2015-11-22 UTC')));
+
+// pocket watch synced to PC on the second at 2016-03-14T11:16:00Z
